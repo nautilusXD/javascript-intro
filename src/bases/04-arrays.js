9fb7ab3f97fe645892867952616e7f90d78ad99f
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-21 18:40:20 -0500

    se agrego archivo 04-Arrays

---
+++ src/bases/04-arrays.js
@@ -0,0 +1,12 @@
+const arreglo = [1, 2, 3, 4]
+arreglo.push(5)
+
+const arreglo2 = [...arreglo]
+
+arreglo2.push(6)
+
+const arreglo3 = arreglo2.map((n) => { return n * 2 })
+
+console.log(arreglo)
+console.log(arreglo2)
+console.log(arreglo3)
