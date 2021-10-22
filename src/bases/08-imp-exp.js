5f48c6b480994ba4ade32cd67cee9167fed4322a
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-22 14:20:25 -0500

    Update 08-imp-exp.js

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,7 +1,5 @@
-import superHeroes from "./data/heroes"
+import superHeroes from "../data/heroes.js"
 
-const getHeroById = (id) => superHeroes.find(hero => hero.id === id)
-console.log(getHeroById(2))
+export const getHeroById = (id) => superHeroes.find(hero => hero.id === id)
 
-const geroHeroByOwner = (owner) => superHeroes.filter(hero => hero.owner === owner)
-console.log(geroHeroByOwner('DC'))
+export const geroHeroByOwner = (owner) => superHeroes.filter(hero => hero.owner === owner)
