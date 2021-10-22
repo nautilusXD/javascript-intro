3cd903c5d0563a2e7931949602174304ebf2605d
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-22 17:11:31 -0500

    Update 11-axios.js

--- src/bases/11-axios.js
+++ src/bases/11-axios.js
@@ -9,10 +9,12 @@ export const giphyApi = axios.create({
     },
 });
 
-giphyApi.get("/random").then((resp) => {
-    const { data } = resp.data;
-    const { url } = data.images.original;
-    const img = document.createElement("img");
-    img.src = url;
-    document.body.append(img);
-});
+export default giphyApi;
+
+// giphyApi.get("/random").then((resp) => {
+//     const { data } = resp.data;
+//     const { url } = data.images.original;
+//     const img = document.createElement("img");
+//     img.src = url;
+//     document.body.append(img);
+// });
